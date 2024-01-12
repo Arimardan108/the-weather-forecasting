97e55e28c18cb2cd075c82fcbf5a27db02f1a6aa
Arimardan108 <mishraarimardan934@gmail.com>
2024-01-12 22:08:30 +0530

    update readme.md

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'd0defdfe8ee83e9b05a6c5630b783f15';
+const WEATHER_API_KEY = 'WEATHER_API_KEY';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
